efa297f7142dbb477bd6f6bebd2d3710762930d0
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-03-10 05:40:28 -0300

    feat: add determine who is in the room function

--- src/pages/Dashboard/Hotel/BookingInformation.js
+++ src/pages/Dashboard/Hotel/BookingInformation.js
@@ -1,6 +1,7 @@
 import { HotelPreview } from './style';
 
 export default function BookingInformation({ booking }) {
+  console.log(booking);
   function determineRoomType(capacity) {
     if (capacity === 1) {
       return 'Single';
@@ -11,6 +12,12 @@ export default function BookingInformation({ booking }) {
     }
   }
 
+  function determineWhoIsTheRoom() {
+    const occupiedVacancies = booking.Room.Booking.length;
+
+    return `Vocês e mais ${occupiedVacancies - 1}`;
+  }
+
   return (
     <HotelPreview isChosen={true}>
       <img src={booking.Room.Hotel.image} alt="hotel_picture" />
@@ -18,7 +25,7 @@ export default function BookingInformation({ booking }) {
       <h2>Quarto Reservado: </h2>
       <p>{`${booking.Room.name} (${determineRoomType(booking.Room.capacity)})`}</p>
       <h2>Pessoas no seu quarto: </h2>
-      <p>Você e mais um</p>
+      <p>{determineWhoIsTheRoom()}</p>
     </HotelPreview>
   );
 }
