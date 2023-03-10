494b039ecdf8544c2a3af61cc8056f547d0ff40c
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-03-10 05:32:09 -0300

    feat: add function to determine the room type

--- src/pages/Dashboard/Hotel/BookingInformation.js
+++ src/pages/Dashboard/Hotel/BookingInformation.js
@@ -1,12 +1,22 @@
 import { HotelPreview } from './style';
 
 export default function BookingInformation({ booking }) {
+  function determineRoomType(capacity) {
+    if (capacity === 1) {
+      return 'Single';
+    } else if (capacity === 2) {
+      return 'Double';
+    } else if (capacity === 3) {
+      return 'Triple';
+    }
+  }
+
   return (
     <HotelPreview isChosen={true}>
       <img src={booking.Room.Hotel.image} alt="hotel_picture" />
       <h1> {booking.Room.Hotel.name} </h1>
       <h2>Quarto Reservado: </h2>
-      <p>101 (triple)</p>
+      <p>{`${booking.Room.name} (${determineRoomType(booking.Room.capacity)})`}</p>
       <h2>Pessoas no seu quarto: </h2>
       <p>Você e mais um</p>
     </HotelPreview>
