611fc1fafe61cd86a1dcd2469c7bf94a71db6cae
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-03-20 02:49:42 -0300

    feat: add conditional rendering in the activities component

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,4 +1,4 @@
-import { DataFilter, Page } from './style';
+import { DataFilter, Page, NoPayment } from './style';
 import useActivity from '../../../hooks/api/useActivity';
 import { useState } from 'react';
 import DateActivity from './DateActivity';
@@ -14,7 +14,6 @@ export default function Activities() {
   const [activities, setActivities] = useState(null);
   const [loading, setLoading] = useState(false);
   const [update, setUpdate] = useState(true);
-
   const token = useToken();
 
   useEffect(() => {
@@ -36,47 +35,53 @@ export default function Activities() {
     return <div>Loading...</div>;
   }
 
-  // if (activitiesData === null) {
-  //   return (
-  //     <Page>
-  //       <div>
-  //         <h1> Escolha de atividades </h1>
-  //       </div>
-  //       <NoPayment>
-  //         <h3>
-  //           Você precisa ter confirmado pagamento antes <br /> de fazer a escolha de atividades
-  //         </h3>
-  //       </NoPayment>
-  //     </Page>
-  //   );
-  // } else {
+  if (activitiesData === null) {
+    return (
+      <Page>
+        <div>
+          <h1> Escolha de atividades </h1>
+        </div>
+        <NoPayment>
+          <h3>
+            Você precisa ter confirmado pagamento antes <br /> de fazer a escolha de atividades{' '}
+          </h3>
+        </NoPayment>
+      </Page>
+    );
+  }
+
   return (
     <Page>
       <div>
         <h1> Escolha de atividades </h1>
       </div>
-      {/* {activitiesData.ticketType ? (
-        <h2> Você não precisa escolher as atividades! </h2>
-      ) : ( */}
-      <div>
-        <h2> Primeiro, filtre pelo dia do evento: </h2>
-        <DataFilter>
-          {activitiesData.dates.map((dateActivity) => (
-            <DateActivity
-              key={dateActivity.id}
-              dateActivity={dateActivity}
-              dateId={dateId}
-              setDateId={setDateId}
-              setShowActivities={setShowActivities}
-              setLoading={setLoading}
-            />
-          ))}
-        </DataFilter>
-        {showActivities && (
-          <ActivitiesContainer loading={loading} activities={activities} update={update} setUpdate={setUpdate} />
-        )}
-      </div>
-      {/* )} */}
+      {activitiesData.isRemote === true && (
+        <NoPayment>
+          <h3>
+            Você precisa ter confirmado pagamento antes <br /> de fazer a escolha de atividades //{' '}
+          </h3>
+        </NoPayment>
+      )}
+      {activitiesData.isRemote === false && (
+        <div>
+          <h2> Primeiro, filtre pelo dia do evento: </h2>
+          <DataFilter>
+            {activitiesData.dates.map((dateActivity) => (
+              <DateActivity
+                key={dateActivity.id}
+                dateActivity={dateActivity}
+                dateId={dateId}
+                setDateId={setDateId}
+                setShowActivities={setShowActivities}
+                setLoading={setLoading}
+              />
+            ))}
+          </DataFilter>
+          {showActivities && (
+            <ActivitiesContainer loading={loading} activities={activities} update={update} setUpdate={setUpdate} />
+          )}
+        </div>
+      )}
     </Page>
   );
 }
